48b4a9b5d82f20be6bfc3481480c06649cb0615f
Beck TJ <bakytbek.tatibekov@gmail.com>
2018-01-03 17:30:58 -0600

    all complete

--- stage-4/src/index.js
+++ stage-4/src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-
+    <BrowserRouter>
     <Provider store={store}>
         <App />
-    </Provider>
+    </Provider> 
+    </BrowserRouter>
     , document.getElementById('root'));
 registerServiceWorker();

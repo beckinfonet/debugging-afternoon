b464caf4ca1f7909ba7de4f16ca3cd8d2ba21052
Beck TJ <bakytbek.tatibekov@gmail.com>
2018-01-03 15:32:51 -0600

    completed

--- stage-1/src/components/StoreFront/StoreFront.js
+++ stage-1/src/components/StoreFront/StoreFront.js
@@ -14,24 +14,28 @@ class StoreFront extends Component {
     componentDidMount() {
         axios.get("https://practiceapi.devmountain.com/products/")
             .then((response) => {
+                console.log(response)
                 this.setState({
-                    products: response
+                    products: response.data
                 })
             })
     }
 
     render() {
-        let productDisplay = this.state.products.map((element, index) => {
-            return (
-                <div className="product-container" key={index}>
-                    <h2>{element.title}</h2>
-                    <img src={element.image} alt="" />
-                    <h2>{element.desc}</h2>
-                    <h3>{"$" + element.price + ".00"}</h3>
-                    <button onClick={() => this.props.addToShoppingCart(element)}>Purchase!</button>
-                </div>
-            )
-        })
+     
+    let productDisplay = [];
+
+            productDisplay = this.state.products.map((element, index) => {
+                return (
+                    <div className="product-container" key={index}>
+                        <h2>{element.title}</h2>
+                        <img src={element.image} alt="" />
+                        <h2>{element.desc}</h2>
+                        <h3>{"$" + element.price + ".00"}</h3>
+                        <button onClick={() => this.props.addToShoppingCart(element)}>Purchase!</button>
+                    </div>
+                )
+            })
         return (
             <div className="storefront-container">
                 {productDisplay}
